6447cea97650cce2817a1b1a2899087af8254588
Abhishek Fatehpuria <abhishekfatehpuria776@gmail.com>
2015-04-29 16:35:19 -0700

    not_used_Vertices

--- algorithm.py
+++ algorithm.py
@@ -1,25 +1,30 @@
 import random
 
-def get_approx_path(edges, colors, N):
+def construct_path(edges, colors, N):
 	used_vertices = set()
+	not_used_vertices = set()
+	for i in range(0,N):
+		not_used_vertices.add(i)
 	path = []
 
 	# First Vertex
 	vertex = randomly_select(N, used_vertices)
 	path = [] + [vertex]
 	used_vertices.add(vertex)
+	not_used_vertices.remove(vertex)
 
 	for i in range(1,N):
 		vertex = randomly_select(N, used_vertices)
-		print vertex
 		path = insertion(edges, color, N, used_vertices, path, vertex)
+		used_vertices.add(vertex)
+		not_used_vertices.remove(vertex)
 
 	return path
 
 
-def randomly_select(N, used_vertices):
+def randomly_select(N, not_used_vertices):
 	num = random.randint(0,N-1)
-	if num not in used_vertices:
+	if num in not_used_vertices:
 		return num
 	else:
 		return randomly_select(N, used_vertices)
