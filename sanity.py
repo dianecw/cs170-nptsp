ac2ef28301d6a2a0cd1dbca9ca7d8390aa267665
Daniel Tsai <daniel_tsai@berkeley.edu>
2015-04-29 17:35:41 -0700

    sanity, weight

--- sanity.py
+++ sanity.py
@@ -4,3 +4,9 @@ def sanity_checker(path, colors):
         color2 = colors[i+1]
         color3 = colors[i+2]
         return "FAIL" if not (color == color2 and color2 == color3)
+
+def weight(path, edges):
+    total = 0
+    for i in range(len(path - 1)):
+        total += edges[path[i]][path[i+1]]
+    return total
