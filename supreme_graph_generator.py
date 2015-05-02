af16b3a497ff6eeb4d9f6b2ae69821f2a8e44ce1
AlexKashi <akashi@berkeley.edu>
2015-05-02 12:59:31 -0700

    fixed indexing bug

--- supreme_graph_generator.py
+++ supreme_graph_generator.py
@@ -44,9 +44,8 @@ def generate_graph(n, fileName):
 			file.write('B')
 			num_blue += 1
 
-
-def crazy_graph(N, fileName, double_prob = .5):
-    best_path = [random.randint(0,50) for _ in range(N-1)] 
+def construct_best_path(N,low = 0 ,high = 100, double_prob = .5):
+    best_path = [random.randint(low,high) for _ in range(N-1)] 
     best_path_colors = [0]*N
     best_path_colors[0] = random.choice([0,1])
     best_path_colors[1] = best_path_colors[0] if random.random() < double_prob else 1- best_path_colors[0]
@@ -90,25 +89,39 @@ def crazy_graph(N, fileName, double_prob = .5):
                 num_red +=1
             else:
                 num_blue +=1
+    return (best_path, best_path_colors)
+
+def crazy_graph(N, fileName, double_prob = .5):
+    best_path, best_path_colors = construct_best_path(N, 0, 50, double_prob)
     distance_lookup_table = [[0 for x in range(N)] for x in range(N)]
     distance_lookup_table[0][1] = best_path[0]
     print(sum(best_path))
-    for i in range(0,N):
-        for j in range(i+1,N):
-            distance_lookup_table[i][j] =  distance_lookup_table[i][j-1] + best_path[j-1]
-            distance_lookup_table[j][i] = distance_lookup_table[i][j]
+    #for i in range(0,N):
+     #   for j in range(i+1,N):
+      #      distance_lookup_table[i][j] =  distance_lookup_table[i][j-1] + best_path[j-1]
+       #     distance_lookup_table[j][i] = distance_lookup_table[i][j]
+
     edge_matrix = [[0 for x in range(N)] for x in range(N)]
     for i in range(N):
         for j in range(i+1,N):
             if (abs(i - j) <= 1):
                 edge_matrix[i][j] = best_path[i]
-            minedge = max(best_path[i-1], best_path[j-1])
+            color_triple = False
+            if( i-1 < 0):
+                minedge = best_path[i-1]
+                color_triple = best_path_colors[i-1] == best_path_colors[i]
+            if (j- 1 > 0):
+                minedge =max(minedge, best_path[j-1])
             if (i < N-2):
                 minedge = max(minedge,best_path[i])
             if (j < N -2):
                 minedge = max(minedge,best_path[j])
-            if(random.random() < .1):
-                minedge -= 10
+                color_triple = color_triple or best_path_colors[j-1] == best_path_colors[j]
+           # if best_path_colors[i] == best_path_colors[j] and color_triple and minedge > 0:
+           #     minedge *= (minedge - 10)//minedge
+           # else:
+            #    if(random.random() < .02 and minedge > 0):
+            #        minedge *= (minedge - 10)//minedge
             minedge = max(0, minedge)
             edge_matrix[i][j] = random.randint(minedge,100)
             edge_matrix[j][i] = edge_matrix[i][j]
